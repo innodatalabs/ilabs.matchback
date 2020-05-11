c6167df9962d60b0b0d3a6fe3d3767b17d9aaa9d
Mike Kroutikov <mkroutikov@innodata.com>
2020-05-11 14:57:41 -0400

    version bump for release

--- ilabs/matchback/__init__.py
+++ ilabs/matchback/__init__.py
@@ -1,4 +1,4 @@
-__version__      = '0.1.0'
+__version__      = '0.1.2'
 __url__          = 'https://github.com/innodatalabs/ilabs.matchback'
 __author__       = 'Mike Kroutikov'
 __author_email__ = 'mkroutikov@innodata.com'
